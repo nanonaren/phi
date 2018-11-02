765d2717f960c2cf9d740b2d036d8ae1ca99eacb
Naren Sundaravaradan <naren@digitalaristotle.ai>
2018-11-02 22:50:14 +0530

    Trying to decide if I should use repa

--- src/Model.hs
+++ src/Model.hs
@@ -19,3 +19,8 @@ for 1 <= i <= N
     for 1 <= s <= 2
       value <- Poisson(lambda_c_s)
 -}
+
+-- op x y = computeS $ fromFunction sh f
+--   where f (Z :. c :. 0) = y ! (Z :. c :. 0) + max 0 (x ! (Z :. c :. 0) - y ! (Z :. c :. 1))
+--         f (Z :. c :. 1) = x ! (Z :. c :. 1) + max 0 (y ! (Z :. c :. 1) - x ! (Z :. c :. 0))
+
