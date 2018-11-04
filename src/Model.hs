64bf68a3c6e3e9a3f999b47f740430a1a873aea0
Naren Sundaravaradan <naren@digitalaristotle.ai>
2018-11-04 21:00:22 +0530

    Going to use Array

--- src/Model.hs
+++ src/Model.hs
@@ -6,11 +6,29 @@ module Model
 import           Data.HashMap.Strict as H
 import           Data.Text           as T
 
-data Model = Model
+data Lambda = LambdaF (Array (Int, Int, Int) Int)
+  | LambdaM (IOArray (Int, Int, Int) Int)
+
+data Frozen
+data Thawed
+
+data Model a = Model
   {
     numColours :: Int
   , token2Id   :: H.HashMap T.Text Int
+  , lambda     :: Lambda
   }
+
+
+new :: Int -> Int -> IO (Model Thawed)
+new maxEmbeddings numC = (Model num H.empty . LambdaM)
+  <$> newArray ((0, 0, 0), (maxEmbeddings, numC, 2)) 0
+
+
+addSample :: Model Thawed -> T.Text -> IO (UArray (Int, Int) Int)
+addSample model txt = do
+  undefined
+
 
 {-
 lambda1+,..., lambdaN+, lambda1-,..., lambdaN- <- Gamma(alpha, beta)
@@ -20,7 +38,9 @@ for 1 <= i <= N
       value <- Poisson(lambda_c_s)
 -}
 
--- op x y = computeS $ fromFunction sh f
---   where f (Z :. c :. 0) = y ! (Z :. c :. 0) + max 0 (x ! (Z :. c :. 0) - y ! (Z :. c :. 1))
---         f (Z :. c :. 1) = x ! (Z :. c :. 1) + max 0 (y ! (Z :. c :. 1) - x ! (Z :. c :. 0))
+-- op x y = listArray (bounds x) $ do
+--   c <- [0..nc-1]
+--   [((c, 0), y ! (c, 0) + max 0 (x ! (c, 0) - y ! (c, 1))),
+--    ((c, 1), x ! (c, 1) + max 0 (y ! (c, 1) - x ! (c, 0)))]
+--   where (nc, 2) = bounds x
 
